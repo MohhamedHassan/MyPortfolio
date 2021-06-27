9c29d430c8d428792e5a0e0f8cfb58f31eef865c
Mohamed Hassan <mohamed>
2021-06-27 16:58:26 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'about',
+    loadChildren:() => import('src/app/view/about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path:'contacts',
+    loadChildren:() => import('src/app/view/contacts/contacts.module').then(m => m.ContactsModule)
+  },
+  {
+    path:'projects',
+    loadChildren:() => import('src/app/view/projects/projects.module').then(m => m.ProjectsModule)
+  },
+  {
+    path:'skils',
+    loadChildren:() => import('src/app/view/skils/skils.module').then(m => m.SkilsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/view/projects/projects/projects.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+  modalOne:boolean=false
+  modalTwo:boolean=false
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+}
